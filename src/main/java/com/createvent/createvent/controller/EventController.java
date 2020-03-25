abc02a4b2c66f5f50821f51fd07ac059195b63f0
tomkurzynski <tomasz.kurzynski@gmail.com>
2020-03-25 17:54:18 +0000

    update

--- src/main/java/com/createvent/createvent/controller/EventController.java
+++ src/main/java/com/createvent/createvent/controller/EventController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.createvent.createvent.dao.EventRepository;
@@ -14,17 +15,18 @@ import com.createvent.createvent.entity.Event;
 
 @RestController
 @CrossOrigin(origins = "*")
+//@RequestMapping(value = "/events")
 public class EventController {
 	
 	@Autowired
 	EventRepository eventRepository;
 	
-	@GetMapping("/events")
+	@GetMapping(value = "/events")
 	public List<Event> getEvents() {
 		return (List<Event>) eventRepository.findAll();
 	}
 	
-	@PostMapping("/events")
+	@PostMapping(value = "/events")
 	void addEvent(@RequestBody Event event) {
 		eventRepository.save(event);
 	}
