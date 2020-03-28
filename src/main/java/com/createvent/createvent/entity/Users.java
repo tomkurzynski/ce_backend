f8a92ce015c904b98d68caf81c246785a4786cb8
tomkurzynski <tomasz.kurzynski@gmail.com>
2020-03-28 19:50:17 +0000

    update

--- src/main/java/com/createvent/createvent/entity/Users.java
+++ src/main/java/com/createvent/createvent/entity/Users.java
@@ -1,9 +1,7 @@
 package com.createvent.createvent.entity;
 
 import java.util.List;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,10 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.createvent.createvent.dto.FestivalDto;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -37,10 +31,9 @@ public class Users {
 	@Column(name = "password")
 	private String password;
 	
-	@Column(name = "email")
 	private String email;
 	
 	
 	@OneToMany(mappedBy = "user")
-	private List<FestivalDto> festival;
+	private List<Festival> festival;
 }
