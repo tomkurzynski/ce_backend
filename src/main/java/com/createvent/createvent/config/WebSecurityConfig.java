f79af968a2360fdbb2e58a7fb974a7e60bd65e70
tomkurzynski <tomasz.kurzynski@gmail.com>
2020-04-06 20:42:20 +0100

    spring security

---
+++ src/main/java/com/createvent/createvent/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.createvent.createvent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http	
+			.authorizeRequests()
+				.antMatchers("/api/**").permitAll()
+				.antMatchers(HttpMethod.POST, "/api").hasRole("ADMIN")
+			    .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+			    .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+			    .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("Admin")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+		}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
