5aafbd4f99723eea3eaf7192ce5bc68809458644
tomkurzynski <tomasz.kurzynski@gmail.com>
2020-04-17 19:32:51 +0100

    security - code update

--- src/main/java/com/createvent/createvent/config/WebSecurityConfig.java
+++ src/main/java/com/createvent/createvent/config/WebSecurityConfig.java
@@ -16,32 +16,32 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	  @Override
-	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-	        auth.inMemoryAuthentication()
-	                .withUser("user").password("{noop}password").roles("ADMIN");
-
-	    }
+//	  @Override
+//	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//	        auth.inMemoryAuthentication()
+//	                .withUser("user").password("{noop}password").roles("ADMIN");
+//
+//	    }
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http	
-			.authorizeRequests()
-				.antMatchers("/api/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/api").hasRole("ADMIN")
-			    .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
-			    .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
-			    .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
-		}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http	
+//			.authorizeRequests()
+//				.antMatchers("/api/**").permitAll()
+//				.antMatchers(HttpMethod.POST, "/api").hasRole("ADMIN")
+//			    .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+//			    .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+//			    .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+//				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/test")
+//				.permitAll()
+//				.and()
+//			.logout()
+//				.permitAll();
+//		}
 //	
 //	@Bean
 //	@Override
@@ -56,20 +56,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		return new InMemoryUserDetailsManager(user);
 //	}
 	
-//	 @Override
-//	    protected void configure(HttpSecurity http) throws Exception {
-//	        http.csrf().
-//	        disable()
-//	            .authorizeRequests()
-//	            .antMatchers(HttpMethod.GET, "/**").hasRole("USER")
-//	            .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
-//	            .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
-//	            .antMatchers(HttpMethod.PATCH, "/**").hasRole("ADMIN")
-//	            .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
-//	            .permitAll()
-//	            .anyRequest()
-//	            .authenticated()
-//	            .and()
-//	            .httpBasic();
-//	    }
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.csrf().
+	        disable()
+	            .authorizeRequests()
+	            .antMatchers(HttpMethod.OPTIONS, "/**")
+	            .permitAll()
+	            .anyRequest()
+	            .authenticated()
+	            .and()
+	            .httpBasic();
+	    }
 }
